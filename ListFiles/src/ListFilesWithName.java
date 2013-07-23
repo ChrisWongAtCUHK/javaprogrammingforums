be1f9f6a86a7bc176e47fc917838680e9105eee7
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-07-23 11:47:46 +0800

    something like dir /s *.txt

---
+++ ListFiles/src/ListFilesWithName.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+
+public class ListFilesWithName {
+	public static final String dirSeparator = "\\";					// window
+	public static final CharSequence asterisk = "*", target1 = ".", replacement1 = "\\.", target2  = "*", replacement2 = ".*";
+	
+	public static void main(String[] args) {
+		String mainClass = System.getProperty("sun.java.command");
+		
+		// Directory path here
+		String path = "", pattern = "", arg0 = "";
+		if(args.length != 1){
+			// If no, use the current directory as path
+			path = System.getProperty("user.dir");
+		} else {
+		
+			// Check if the input has '*'
+			if (args[0].contains(asterisk)) {
+				// Check if the input has dirSeparator
+				if (args[0].contains(dirSeparator)) {
+					// If yes, split the path and the pattern by dirSeparator
+					String[] strs = args[0].split("\\\\");			// window
+					
+					for(int i = 0; i < strs.length -  2; i++)
+						path += strs[i] + dirSeparator;
+					path += strs[strs.length -  2];
+					pattern = strs[strs.length - 1];
+				} else {
+					// If no, use the current directory as path and use args[0] as pattern
+					path = System.getProperty("user.dir");
+					pattern = args[0];
+				}
+				
+				// Replacement for regular expression
+				pattern = pattern.replace(target1, replacement1);
+		        pattern = pattern.replace(target2, replacement2);
+			} else {
+				// If no '*', just use args[0] as path
+				path = args[0];
+			}
+		}
+		
+		listRecurrsive(path, pattern);
+	}
+	
+	public static void listRecurrsive(String path, String pattern){
+		
+		File folder = new File(path);
+		// Check if the path exists
+		if(!folder.exists()){
+			System.out.println("No such path--" + path);
+			return;
+		} else if(folder.isFile()){
+			// Special case for input path is file name
+			// Get the last modified timestamp
+			long lastModified = folder.lastModified();
+			Timestamp ts = new Timestamp(lastModified);
+			String date = new SimpleDateFormat("dd/MM/yyyy\thh:mm:ss").format(ts);
+
+			System.out.println(date + "\t" + path);
+			return ; 
+		}
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			// Check if file or directory
+			if (listOfFiles[i].isFile()) {
+				// Check if the pattern match
+				
+				String fileName = listOfFiles[i].getName();
+				if((!pattern.equals("")) &&  !fileName.matches(pattern)){
+					continue;
+				}
+				
+				// Get the last modified timestamp
+				long lastModified = listOfFiles[i].lastModified();
+				Timestamp ts = new Timestamp(lastModified);
+				String date = new SimpleDateFormat("dd/MM/yyyy\thh:mm:ss").format(ts);
+				
+				
+				System.out.println(date + "\t" + path + dirSeparator + fileName);			// window
+			} else if(listOfFiles[i].isDirectory()){
+				String subdir = listOfFiles[i].toString();
+				listRecurrsive(subdir, pattern);
+			}
+		}		
+	}
+
+}
